43261bc10addbf2f1ae4fab99d1a2f5429f3c80a
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-03-29 18:01:39 -0600

    Added Basic JavaScript project

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,15 @@
+//window.alert method
+window.alert("Hello, world!");
+
+//document.write method
+document.write('How are you today?');
+
+//two variables
+var sent1 = "This is the beginning of the string";
+var sent2 = " and this is the end of the string.";
+
+//concatenating variables
+document.write(sent1 + sent2);
+
+//expression
+document.write(7 + 10);
