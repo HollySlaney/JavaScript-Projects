69ff9745323b1913c818062f544fa7e3d01f164c
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-04-01 21:42:08 -0600

    Added Project2_Functions Assignment HTML and JS

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//a function that uses += operator
+function displayMessage() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
