f7e13573490c26250d703c11a2de41d2dca8ca6b
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-04-26 20:51:10 -0600

    Added JavaScript Projects Folder

---
+++ Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,50 @@
+function Color_Function() {
+    var Color_Output;
+    var Colors = document.getElementById("Color_Input").value;
+    var Color_String = " is a great color!";
+    switch(Colors) {
+        case "Red":
+            Color_Output = "Red" + Color_String;
+        break;
+        case "Yellow":
+            Color_Output = "Yellow" + Color_String;
+        break;
+        case "Green":
+            Color_Output = "Green" + Color_String;
+        break;
+        case "Blue":
+            Color_Output = "Blue" + Color_String;
+        break;
+        case "Pink":
+            Color_Output = "Pink" + Color_String;
+        break;
+        case "Purple":
+            Color_Output = "Purple" + Color_String;
+        break;
+        default:
+            Color_Output = "Please enter a color exactly as written on the above list.";
+    }
+    document.getElementById("Output").innerHTML = Color_Output;
+}
+
+//.getElementsByClass() method
+function Hello_World_Function() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "The text has changed!";
+}
+
+//Canvas challenge
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+//Gradient canvas
+const c = document.getElementById("myGradientCanvas");
+const ctx = c.getContext("2d");
+const grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
